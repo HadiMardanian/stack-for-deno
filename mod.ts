85513715d54aa5ce2ac5f9e1e7f054c20cb45804
hadi <mardanianhadi@gmail.com>
2021-02-05 22:11:24 +0330

    new

---
+++ mod.ts
@@ -0,0 +1,35 @@
+export default class Stack{
+    private array: Array<any>;
+    private topIndex: number;
+    
+    public constructor(){
+        this.array = [];
+        this.topIndex = -1;
+    }
+
+    public size() {
+        return this.topIndex + 1;
+    }
+
+    public push(x: any): Stack{
+        this.array[++this.topIndex] = x;
+        return this;
+    }
+
+    public pop(): any{
+        return this.array[this.topIndex--];
+    }
+    
+    public toString(): string{
+        return `[${this.array.toString()}]`;
+    }
+
+    public isEmpty(): Boolean{
+        return this.topIndex == -1;
+    }
+
+    public clear(){
+        this.topIndex = -1;
+    }
+    
+}
